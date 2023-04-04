fbd293471bd02be62b4fd0cf52fe24d42b48f6ea
AlexBatya <alex28.12.12@mail.ru>
2023-04-05 00:52:42 +1000

    'new'

---
+++ js/slider.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $(".sliderHeader").slick({
+        slidesToShow: 4,    //сколько картинок в одном показе
+        slidesToScroll: 1,    //Сколько картинок перелистывается за раз 
+        // centerPadding: '60px',   
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {  
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+            },
+            {
+            breakpoint: 425,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+            }
+        ],  
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: false,
+        focusOnSelect: true,
+    });
+    $(".sliderAnter").slick({
+        slidesToShow: 3,    
+        slidesToScroll: 1,    
+        // centerPadding: '60px',   
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {  
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+            },
+            {
+            breakpoint: 425,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+            }
+        ],  
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        // arrows: false,
+        focusOnSelect: true,
+    });
+});
